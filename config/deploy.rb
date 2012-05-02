10952352c6b204eb43673779977fab4f1830903e
Kevin Lawver <lawver@gmail.com>
2012-05-02 14:56:28 -0400

    initial commit

---
+++ config/deploy.rb
@@ -0,0 +1,1 @@
+server '192.168.8.128', :app, :web, :db, :primary => true
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moonbounce::Application.config.secret_token = 'eae5bad2dc71ad18f4c315fd843aa1417cccbd27d2fefa4a40834e39ce3a263d7b43bd52cc32f5068512e2d96f0aab5b57585170d2f38feda9b4c8ddec50a98c'
